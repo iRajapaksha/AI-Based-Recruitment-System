c2168a50c84cdde6435ba1ef582f9ab68e09329b
iRajapaksha <nayanajith.ishara2541@gmail.com>
2025-07-25 21:35:52 +0530

    starting with microservices

---
+++ jobpost-service/src/main/java/com/recruitment_system/jobpost_service/JobpostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recruitment_system.jobpost_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobpostServiceApplication.class, args);
+	}
+
+}
