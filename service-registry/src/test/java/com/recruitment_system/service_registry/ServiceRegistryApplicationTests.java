6c322240f5e3a4dbe7deec992f2b800575c69361
iRajapaksha <nayanajith.ishara2541@gmail.com>
2025-07-26 12:14:02 +0530

    service registry implemented

---
+++ service-registry/src/test/java/com/recruitment_system/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment_system.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
