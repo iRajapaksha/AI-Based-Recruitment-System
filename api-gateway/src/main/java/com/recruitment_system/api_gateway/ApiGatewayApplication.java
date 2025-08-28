e9d3676951fdcba42aa641b112716642af913b91
iRajapaksha <nayanajith.ishara2541@gmail.com>
2025-08-28 13:20:14 +0530

    request mapping for the microservices

--- api-gateway/src/main/java/com/recruitment_system/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/recruitment_system/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.recruitment_system.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
