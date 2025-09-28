c4ecd02a886a45d1db351a317383b7b0c56ed2e6
iRajapaksha <nayanajith.ishara2541@gmail.com>
2025-09-28 22:19:10 +0530

    jwt expiration extended

--- auth-service/src/main/java/com/recruitment_system/auth_service/security/JwtUtil.java
+++ auth-service/src/main/java/com/recruitment_system/auth_service/security/JwtUtil.java
@@ -20,7 +20,7 @@ public class JwtUtil {
     private final String SECRET_KEY = "sectretkey123werthyjh24356ujhgfewqrgthgnbvdewrgthnwergthyjnbsehrtytsgratthrjyndgbfrathydgfbvd";
     SecretKey key = new SecretKeySpec(SECRET_KEY.getBytes(), SignatureAlgorithm.HS512.getJcaName());
 
-    private final long EXPIRATION = 1000*60*60;
+    private final long EXPIRATION = 1000*60*60*24;
 
     public String generateToken(String email, Role role) {
         return Jwts.builder()
