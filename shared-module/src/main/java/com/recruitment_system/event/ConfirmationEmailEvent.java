23530e8d748f4decdaa0d58892feb7d6d18decfb
iRajapaksha <nayanajith.ishara2541@gmail.com>
2025-10-28 12:59:27 +0530

    added shared event for confirmation email

--- shared-module/src/main/java/com/recruitment_system/event/ConfirmationEmailEvent.java
+++ shared-module/src/main/java/com/recruitment_system/event/ConfirmationEmailEvent.java
@@ -1,2 +1,21 @@
-package com.recruitment_system.event;public class ConfirmationEmailEvent {
+package com.recruitment_system.event;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ConfirmationEmailEvent implements Serializable {
+    private Long applicationId;
+    private Long jobPostId;
+    private String email;
+    private String subject;
+    private String body;
+//    private String correlationId; // UUID for tracing
+//    private Map<String, String> metadata; // optional
 }
